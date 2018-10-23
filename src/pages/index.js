f30675ecfd63afe110db3167b8c326a00215f36e
nehasoni <neha.soni1908@gmail.com>
2018-10-23 14:23:38 +0530

    test commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,6 @@ import Layout from '../components/layout'
 const IndexPage = () => (
   <Layout>
     <h1>Hi</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
